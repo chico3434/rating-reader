1faaaad0bebd945389eb18ccb56ee0a239edd5c4
chico3434 <chicokarkow@hotmail.com>
2019-03-30 08:53:15 -0300

    Protótipo de Main criado

--- src/br/cefet/ratingreader/Main.java
+++ src/br/cefet/ratingreader/Main.java
@@ -1,7 +1,15 @@
 package br.cefet.ratingreader;
 
+import br.cefet.ratingreader.reader.CSVReader;
+
 public class Main {
     public static void main(String[] args) {
-
+        CSVReader.openCSV("/home/francisco/Downloads/ListaObraDadosAbertos.csv");
+        while (true) {
+            String[] data = CSVReader.readCSV(1, 16);
+            if (data != null) {
+                System.out.println("Nome: " + data[0] + " Classificação: " + data[1]);
+            }
+        }
     }
 }
