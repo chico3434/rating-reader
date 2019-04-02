e40fd8823a9e4d22f1b8c2f4511d109c8400c84b
chico3434 <chicokarkow@hotmail.com>
2019-04-02 08:12:37 -0300

    Problema corrigido em PathSearch e agora filtra por Longa Metragem

--- src/br/cefet/ratingreader/search/PathSearch.java
+++ src/br/cefet/ratingreader/search/PathSearch.java
@@ -9,18 +9,34 @@ import java.util.List;
 public class PathSearch {
 
     public static List<String[]> searchByPath(String path) {
-        Reader reader = new Reader(path);
+        List<String> movies = getMovieToBeSearch(path);
         List<String[]> listOfResults = new ArrayList<>();
+        System.out.println("Buscando...");
         while (CSVReader.hasNext()) {
             String[] data = CSVReader.readCSV();
-            while(reader.hasNext()) {
-                String title = reader.readLine().trim().toLowerCase();
-                if (data[1].trim().toLowerCase().equals(title)) {
+            System.out.print(".");
+            if (movies.contains(data[1].trim().toLowerCase())) {
+                if (data[10].trim().equals("Longa Metragem")) {
+                    System.out.println("\nAchado: " + data[1]);
                     listOfResults.add(data);
                 }
             }
+
         }
+        System.out.println("Busca terminada");
         return listOfResults;
+    }
+
+    public static List<String> getMovieToBeSearch(String path) {
+        List<String> movies = new ArrayList<>();
+        Reader reader = new Reader(path);
+        System.out.println("Lendo filmes");
+        while (reader.hasNext()) {
+            System.out.print(".");
+            movies.add(reader.readLine().trim().toLowerCase());
+        }
+        System.out.println("Leitura terminada");
+        return movies;
     }
 
 }
