6acc746a63654da6a9f6c563cecba6dda88f2d5b
chico3434 <chicokarkow@hotmail.com>
2019-04-02 08:32:58 -0300

    Lógica do Main implementada para que possa ler o caminho do CSV, caminho ou nome dos filmes a ser buscado, e o arquivo de salvamento.

--- src/br/cefet/ratingreader/Main.java
+++ src/br/cefet/ratingreader/Main.java
@@ -1,14 +1,24 @@
 package br.cefet.ratingreader;
 
 import br.cefet.ratingreader.reader.CSVReader;
+import br.cefet.ratingreader.search.PathSearch;
 import br.cefet.ratingreader.search.Search;
+import br.cefet.ratingreader.writer.Writer;
+
+import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
         String path = "";
         String movieSearch = "";
         String pathSearch = "";
         String out = "";
+        List<String[]> result = null;
+        String[] uniqueResult = new String[0];
         for(int i = 0; i < args.length; i++) {
             if (args[i].contains("--path=")) {
                 path = args[i].replace("--path=", "");
@@ -24,13 +34,51 @@ public class Main {
             }
         }
 
-        CSVReader.openCSV(path);
-
-        if (!pathSearch.equals("")) {
-
+        if (!path.equals("")) {
+            CSVReader.openCSV(path);
+        } else {
+            System.out.println("Entre com o caminho: ");
+            path = sc.nextLine();
+            CSVReader.openCSV(path);
         }
 
-        if (!movieSearch.equals("")) {
+        if (pathSearch.equals("")) {
+            System.out.println("Entre com o caminho do arquivo: ");
+            System.out.println("Caso queira ler um só filme digite \'skip\'");
+            pathSearch = sc.nextLine();
+            if (!pathSearch.trim().toLowerCase().equals("skip")) {
+                result = PathSearch.searchByPath(path);
+                if (result != null) {
+                    for (String[] movie : result) {
+//                        for (String string : movie) {
+//                            System.out.println(string);
+//                        }
+                        System.out.printf("Nome: %s - Classificação %s\n", movie[1], movie[16]);
+                    }
+                } else {
+                    System.out.println("Algum erro ocorreu");
+                }
+            } else {
+                pathSearch = "";
+            }
+        }
+
+        if (movieSearch.equals("")) {
+            if (pathSearch.equals("")) {
+                System.out.println("Entre com o filme a ser buscado: ");
+                movieSearch = sc.nextLine();
+                uniqueResult = Search.searchByName(movieSearch);
+                if (uniqueResult != null) {
+                    for (String string : uniqueResult) {
+                        System.out.println(string);
+                    }
+                } else {
+                    System.out.println(uniqueResult);
+                }
+            }
+
+        } else {
+            System.out.println(movieSearch);
             String data[] = Search.searchByName(movieSearch);
             if (data != null) {
                 for (String s : data) {
@@ -38,6 +86,33 @@ public class Main {
                 }
             } else {
                 System.out.println(data);
+            }
+        }
+
+        if (out.equals("")) {
+            System.out.println("Deseja salvar?");
+            String response = sc.nextLine();
+            if (response.equals("sim")) {
+                System.out.println("Onde deseja salvar?");
+                out = sc.nextLine();
+
+            } else {
+                System.exit(0);
+            }
+            if (result != null) {
+                if (Writer.write(out, result)) {
+                    System.out.println("Salvo com sucesso!");
+                } else {
+                    System.out.println("Erro ao salvar!");
+                }
+            } else {
+                if (uniqueResult.length > 0) {
+                    if (Writer.write(out, uniqueResult)) {
+                        System.out.println("Salvo com sucesso!");
+                    } else {
+                        System.out.println("Erro ao salvar!");
+                    }
+                }
             }
         }
     }
