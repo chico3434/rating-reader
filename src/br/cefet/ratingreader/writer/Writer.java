1ee8a9f131b00631e5ef93de9d026605d6892ad3
chico3434 <chicokarkow@hotmail.com>
2019-04-07 19:39:16 -0300

    Writer reescrito

--- src/br/cefet/ratingreader/writer/Writer.java
+++ src/br/cefet/ratingreader/writer/Writer.java
@@ -3,30 +3,20 @@ package br.cefet.ratingreader.writer;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.util.List;
+import java.util.Iterator;
+import java.util.Set;
 
 public class Writer {
 
-    public static boolean write(String path, List<String[]> result) {
+    public static boolean write(String path, Set<String[]> result) {
         System.out.println("Salvando...");
         try {
             BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path));
-            for (String[] movie : result) {
-                bufferedWriter.append(movie[1] + " @ " + movie[16] + "\n");
+            Iterator<String[]> iterator = result.iterator();
+            while(iterator.hasNext()) {
+                String[] movie = iterator.next();
+                bufferedWriter.append(movie[0] + "," + movie[1]+ "\n");
             }
-            bufferedWriter.close();
-            return true;
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return false;
-    }
-
-    public static boolean write(String path, String[] result) {
-        System.out.println("Salvando...");
-        try {
-            BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(path));
-            bufferedWriter.write(result[1] + " @ " + result[16] + "\n");
             bufferedWriter.close();
             return true;
         } catch (IOException e) {
