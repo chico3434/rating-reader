27690d07a421c876123811bf56bb74cbf3634251
chico3434 <chicokarkow@hotmail.com>
2019-04-07 19:39:39 -0300

    Main reformulado

--- src/br/cefet/ratingreader/Main.java
+++ src/br/cefet/ratingreader/Main.java
@@ -1,138 +1,43 @@
 package br.cefet.ratingreader;
 
 import br.cefet.ratingreader.reader.CSVReader;
-import br.cefet.ratingreader.search.PathSearch;
 import br.cefet.ratingreader.search.Search;
-import br.cefet.ratingreader.writer.FormattedWriter;
+import br.cefet.ratingreader.utils.Args;
+import br.cefet.ratingreader.utils.Console;
+import br.cefet.ratingreader.utils.Values;
 import br.cefet.ratingreader.writer.Writer;
 
-import java.util.List;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
 
     static Scanner sc = new Scanner(System.in);
 
     public static void main(String[] args) {
-        // formatted é a variável que indicará se o resultado vai sair no formato padrão: Nome, Número da classificação
-        boolean formatted = false;
-        String path = "";
-        String movieSearch = "";
-        String pathSearch = "";
-        String out = "";
-        List<String[]> result = null;
-        String[] uniqueResult = new String[0];
-        for(int i = 0; i < args.length; i++) {
-            if (args[i].contains("--path=")) {
-                path = args[i].replace("--path=", "");
-            }
-            if (args[i].contains("--search-movie=")) {
-                movieSearch = args[i].replace("--search-movie=", "").replace("_", "");
-            }
-            if (args[i].contains("--search-path=")) {
-                pathSearch = args[i].replace("--search-path=", "");
-            }
-            if (args[i].contains("--out=")) {
-                out = args[i].replace("--out=", "");
-            }
-            if (args[i].contains("--formatted")) {
-                formatted = true;
-            }
-        }
-
-        if (!path.equals("")) {
-            CSVReader.openCSV(path);
+        Args.readArgs(args);
+        if (Values.source != null) {
+            CSVReader.openCSV(Values.source);
         } else {
             System.out.println("Entre com o caminho: ");
-            path = sc.nextLine();
-            CSVReader.openCSV(path);
+            Values.source = sc.nextLine();
+            CSVReader.openCSV(Values.source);
+        }
+        if (Values.out == null) {
+            System.out.println("Entre com o caminho de saída: ");
+            Values.out = sc.nextLine();
         }
 
-        if (pathSearch.equals("")) {
-            System.out.println("Entre com o caminho do arquivo: ");
-            System.out.println("Caso queira ler um só filme digite \'skip\'");
-            pathSearch = sc.nextLine();
-            if (!pathSearch.trim().toLowerCase().equals("skip")) {
-                result = PathSearch.searchByPath(pathSearch);
-                if (result != null) {
-                    for (String[] movie : result) {
-                        System.out.printf("Nome: %s - Classificação %s\n", movie[1], movie[16]);
-                    }
-                } else {
-                    System.out.println("Algum erro ocorreu");
-                }
-            } else {
-                pathSearch = "";
-            }
-        } else {
-                result = PathSearch.searchByPath(pathSearch);
-                if (result != null) {
-                    for (String[] movie : result) {
-                        System.out.printf("Nome: %s - Classificação %s\n", movie[1], movie[16]);
-                    }
-                } else {
-                    System.out.println("Algum erro ocorreu");
-                }
-        }
-
-        if (movieSearch.equals("")) {
-            if (pathSearch.equals("")) {
-                System.out.println("Entre com o filme a ser buscado: ");
-                movieSearch = sc.nextLine();
-                uniqueResult = Search.searchByName(movieSearch);
-                if (uniqueResult != null) {
-                    for (String string : uniqueResult) {
-                        System.out.println(string);
-                    }
-                } else {
-                    System.out.println(uniqueResult);
-                }
-            }
-
-        } else {
-            System.out.println(movieSearch);
-            String data[] = Search.searchByName(movieSearch);
-            if (data != null) {
-                for (String s : data) {
-                    System.out.println(s);
-                }
-            } else {
-                System.out.println(data);
-            }
-        }
-
-        if (out.equals("")) {
-            System.out.println("Deseja salvar?");
-            String response = sc.nextLine();
-            if (response.equals("sim")) {
-                System.out.println("Onde deseja salvar?");
-                out = sc.nextLine();
-            } else {
-                System.exit(0);
-            }
-        }
-        if (formatted) {
-            if (FormattedWriter.writeSet(out, result)) {
+        Set<String[]> resultSet = Search.searchAndReturnSet(Values.out);
+        if (Values.save) {
+            if (Writer.write(Values.out + "/rating.csv", resultSet)) {
                 System.out.println("Salvo com sucesso!");
             } else {
                 System.out.println("Erro ao salvar!");
             }
-            System.exit(0);
         }
-        if (result != null) {
-            if (Writer.write(out, result)) {
-                System.out.println("Salvo com sucesso!");
-            } else {
-                System.out.println("Erro ao salvar!");
-            }
-        } else {
-            if (uniqueResult.length > 0) {
-                if (Writer.write(out, uniqueResult)) {
-                    System.out.println("Salvo com sucesso!");
-                } else {
-                    System.out.println("Erro ao salvar!");
-                }
-            }
+        if (Values.console) {
+            Console.write(resultSet);
         }
     }
 }
